66328dfd3e7afbd52fbf87b9204c2f5df23b2462
EvgenyTimoshin <evgenytimoshin@gmail.com>
2018-12-17 15:22:24 +0000

    Added objects affected by gravity

---
+++ ProceduralSoundViz/Assets/AffectedByGravity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffectedByGravity : MonoBehaviour {
+    Rigidbody _rb;
+    protected Transform _attractedTo;
+    ///Make this modifiable in the editor
+    protected float _attractionStrenght = 100;
+    protected float _maxPower = 100;
+    protected float _size;
+    // Use this for initialization
+
+    public static AffectedByGravity Create(Vector3 pos, float size, Transform attractedTo) {
+        AffectedByGravity ab = GameObject.CreatePrimitive(PrimitiveType.Sphere).AddComponent<AffectedByGravity>();
+        ab._size = size;
+        ab.transform.localScale = new Vector3(size,size,size);
+        ab._attractedTo = attractedTo;
+
+        return ab;
+    }
+
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_attractedTo) {
+            Vector3 dir = _attractedTo.transform.position - transform.position;
+            _rb.AddForce(_attractionStrenght * dir);
+
+            if (_rb.velocity.magnitude > _maxPower) {
+                _rb.velocity = _rb.velocity.normalized * _maxPower;
+            }
+        }
+		
+	}
+}
