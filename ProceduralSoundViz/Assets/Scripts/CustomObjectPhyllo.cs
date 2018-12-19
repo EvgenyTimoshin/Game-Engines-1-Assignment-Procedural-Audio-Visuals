db48b45d0810c35c0bb4225c29bcf4c861bedc6f
EvgenyTimoshin <evgenytimoshin@gmail.com>
2018-12-19 23:36:22 +0000

    Created Random Phyllo Experience

--- ProceduralSoundViz/Assets/Scripts/CustomObjectPhyllo.cs
+++ ProceduralSoundViz/Assets/Scripts/CustomObjectPhyllo.cs
@@ -6,15 +6,26 @@ public class CustomObjectPhyllo : Phyllotaxis{
     public GameObject _customPrefab;
     [Range(0,10)]
     public float _customObjectSize = 1;
+    public Gradient _gradient;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
         _degreeDiff = GetDegreeDiff(_numOfObjects);
         var n = _startNumber;
         for (int i = 0; i < _numOfObjects; i++)
         {
+            float step = 1.0f / _numOfObjects;
+            Color color = _gradient.Evaluate(step * i);
+
             GameObject newPrefab = Instantiate(_customPrefab);
+            Renderer rend = newPrefab.GetComponent<Renderer>();
+            Material newMat = new Material(rend.material);
+            newMat.color = color;
+            rend.material = newMat;
             IPhylloEffected newObj = CustomObject.Create(newPrefab);
+
+            
+
             _objects.Add(newObj, n);
             n = n + _degreeDiff;
 
@@ -24,6 +35,11 @@ public class CustomObjectPhyllo : Phyllotaxis{
                 SetNewTargetPoses();
             }
         }
+    }
+
+    // Use this for initialization
+    void Start () {
+       
 	}
 	
 	// Update is called once per frame
